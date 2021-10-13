d113a34fadfa37749ae2696c79cd7fd829990d64
tritio <tritio2@gmail.com>
2021-10-13 09:44:36 +0200

    code sonar-scanner

--- strict_equal/index.js
+++ strict_equal/index.js
@@ -9,3 +9,10 @@ function strictEqual(a, b) {
 }
 
 console.log(strictEqual(0, -0));
+
+/* Execute to scanner:
+   sonar-scanner \
+  -Dsonar.projectKey=strict_equal \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://127.0.0.1:9000 \
+  -Dsonar.login=be7b9df2c2e049d2bc029bb9ee6459b35ab73b61 */
